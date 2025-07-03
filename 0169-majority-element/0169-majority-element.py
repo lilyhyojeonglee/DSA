551d8a87b041ab41292a4f4992e425189ff669ac
Lily Hyojeong Lee <hl6vbw@virginia.edu>
2025-07-03 16:22:07 -0400

    Time: 8 ms (62.53%), Space: 13.3 MB (98.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,11 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = {}
-        minnum = len(nums)/2
-        for i in nums:
-            count[i] = count.get(i, 0) + 1
-            if count[i]> minnum:
-                return i
+        # count = {}
+        # minnum = len(nums)/2
+        # for i in nums:
+        #     count[i] = count.get(i, 0) + 1
+        #     if count[i]> minnum:
+        #         return i
+
+        res = nums[0]
+        count = 1
+        for i in nums[1:]:
+            if i == res:
+                count+=1
+            
+            elif i != res and count ==0:
+                res = i
+                count +=1
+            else:
+                count -=1
+        
+        return res
+
+
 
         
